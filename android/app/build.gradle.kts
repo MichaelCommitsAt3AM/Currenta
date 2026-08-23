f4d6dd4a6e90099600bb99d6a6ed12aaceb3b90f
Michael <michaelnjonge93@gmail.com>
2026-08-23 12:48:13 +0300

    fix: update Firebase plugin versions and set keystore store type to PKCS12

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -57,6 +57,7 @@ android {
             keyPassword = keystoreProperties["keyPassword"] as String?
             storeFile = keystoreProperties["storeFile"]?.let { file(it) }
             storePassword = keystoreProperties["storePassword"] as String?
+            storeType = "PKCS12"
         }
     }
     productFlavors {
